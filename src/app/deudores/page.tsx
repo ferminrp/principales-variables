f118938705be49068ef8dca56474e45c6a9e13d7
Fermin Rodriguez Penelas <frodriguezpenelas@gmail.com>
2024-09-26 17:03:27 -0300

    metadata

--- src/app/deudores/page.tsx
+++ src/app/deudores/page.tsx
@@ -1,4 +1,23 @@
+import { Metadata } from 'next'
 import { DeudasBcra } from "@/components/deudas-bcra"
+
+export const metadata: Metadata = {
+  title: "Consulta Gratuita de Deuda | BCRA",
+  description: "Consulta la situación crediticia de personas y empresas en la Central de Deudores del Banco Central de la República Argentina (BCRA)",
+  openGraph: {
+    title: "Consulta de Deudores BCRA",
+    description: "Verifica la situación crediticia en la Central de Deudores del BCRA",
+    type: "website",
+    url: "https://bcra.ferminrp.com/deudores",
+    images: [{ url: "/meta-img.png" }],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Consulta de Deudores BCRA",
+    description: "Verifica la situación crediticia en la Central de Deudores del BCRA",
+    images: ["/meta-img.png"],
+  },
+}
 
 export default function DeudoresPage() {
   return <DeudasBcra />
